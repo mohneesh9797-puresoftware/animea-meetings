81e7091eacc228731b8aedb9e4ae8d8cb0873256
mariaap95 <malcper23@gmail.com>
2019-12-12 18:27:44 +0100

    Removed unused code

--- db.js
+++ db.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-const SERVER = 'localhost:27017';
-const DATABASE = 'meetings';
-const OPTIONS = 'retryWrites=true&w=majority';
-
 function connect() {
     mongoose.connect('mongodb+srv://admin:' + process.env.MONGO_ATLAS_PW + '@animea-meetings-tdrau.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
 }
