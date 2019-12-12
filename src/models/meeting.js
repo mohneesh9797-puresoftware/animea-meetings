eec2f0d219339fc9f5dc60c0d8193695223d823e
Alicia <aliviaord@alum.us.es>
2019-12-12 19:48:38 +0100

    Meeting model completed

--- src/models/meeting.js
+++ src/models/meeting.js
@@ -2,8 +2,28 @@ const mongoose = require('mongoose');
 
 const meetingSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    description: String
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    startingDate: {
+        type: Date,
+        required: true
+    },
+    endingDate: Date,
+    capacity: Number,
+    creatorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Meeting', meetingSchema);
