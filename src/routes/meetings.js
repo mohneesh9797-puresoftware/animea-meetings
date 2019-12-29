cbe3d078cc56a05913f3dc4cb1ee97081011de15
Alicia <aliviaord@alum.us.es>
2019-12-29 23:11:47 +0100

    Functions are now stored in a separated file

--- src/routes/meetings.js
+++ src/routes/meetings.js
@@ -8,6 +8,7 @@ const { Provinces } = require('../models/meeting');
 const profileAxios = axios.create({
     baseURL: 'http://localhost:3001/api/'
 })
+const meetingService = require('../services/meetingService');
 
 const provincesValues = Object.values(Provinces);
 
@@ -91,34 +92,11 @@ router.get('/:meetingId', (req, res, next) => {
 // Devuelve el listado de meetings a los que el
 // usuario con la ID indicada se ha unido.
 
-async function getMeetingsFromUser(userId) {
-
-    try {
-        // Llamada a la API del microservicio Profile para
-        // obtener el listado de IDs de los meetings del
-        // usuario.
-        let doc = await profileAxios.get('user/' + userId + '/joinedMeetings');
-        let meetingsIds = doc.data;
-
-        // Obtiene de la base de datos los meetings cuyo
-        // ID se encuentra en el listado obtenido anteriormente.
-        let meetingsCollection = await Meeting.find().where('_id').in(meetingsIds)
-            .select("_id name description address province postalCode startingDate endingDate capacity creatorId members")
-            .exec();
-        
-        return [false, meetingsCollection];
-    
-    } catch (error) {
-
-        return [true, error];
-    }
-}
-
 router.get('/user/:userId', (req, res, next) => {
 
     var userId = req.params.userId.toString();
 
-    getMeetingsFromUser(userId).then(doc => {
+    meetingService.getMeetingsFromUser(userId).then(doc => {
         if (doc[0]) {
             console.log(doc[1]);
             var statusNumber = 500;
@@ -189,59 +167,6 @@ router.post('/', (req, res, next) => {
 // Elimina el meeting correspondiente a la ID que
 // se indique.
 
-async function deleteMeeting(meetingId) {
-
-    try {
-        var nowDate = new Date(Date.now());
-
-        // TODO: Comprobar que existe un usuario autenticado y
-        // obtener su ID.
-        let fakeUserId = "1";
-
-        // Obtiene de la base de datos el meeting que se
-        // pretende eliminar.
-        let doc = await Meeting.findById(meetingId)
-            .select("_id name description address province postalCode startingDate endingDate capacity creatorId members")
-            .exec();
-        
-        // Comprobar que existe un meeting con la ID proporcionada.
-        if (!doc) {
-            throw "Error 404: We couldn't find any meeting with the given ID."
-
-        // Comprobar que la ID del usuario que está intentando
-        // abandonar el meeting coincide con su creatorId.
-        } else if (doc.creatorId.toString().localeCompare(fakeUserId) != 0) {
-            throw "Error 400: You can't delete a meeting that you didn't create.";
-
-        // Comprobar que la startingDate del meeting es futura
-        // y, por tanto, dicho meeting no ha comenzado.
-        } else if (doc.startingDate <= nowDate) {
-            throw "Error 400: You can't delete the meeting. It has already started.";
-        
-        } else {
-            // El listado de miembros del meeting recoge las dependencias
-            // con los usuarios que se deben actualizar en la base de datos
-            // del microservicio profile.
-            var members = doc.members;
-
-            // Eliminamos el meeting de nuestra base de datos.
-            await Meeting.deleteOne({_id : meetingId}).exec();
-            
-            // Actualizamos las dependencias del microservicio profile,
-            // quitando el meeting eliminado para cada uno de los miembros
-            // en sus respectivos listados de meetings. 
-            for (var i = 0; i < members.length; i++) {
-                await profileAxios.put('user/' + members[i] + '/leavesMeeting/' + meetingId);
-            }
-
-            return [false, members];
-        }
-    
-    } catch (error) {
-        return [true, error];
-    }
-}
-
 router.delete('/:meetingId', (req, res, next) => {
 
     // Comprobar que se pasa una ID válida como parámetro.
@@ -254,7 +179,7 @@ router.delete('/:meetingId', (req, res, next) => {
         // Llamada al método asíncrono principal que se encarga
         // de eliminar el meeting y actualizar las dependencias
         // en el microservicio Profile
-        deleteMeeting(meetingId).then(doc => {
+        meetingService.deleteMeeting(meetingId).then(doc => {
 
             // Comprobar si se ha producido un error (doc[0] = true) y
             // enviar el código y mensaje adecuados.
@@ -292,64 +217,6 @@ router.delete('/:meetingId', (req, res, next) => {
 // listado de asistentes del meeting cuya ID se
 // indica por parámetros.
 
-async function leaveMeeting(meetingId) {
-
-    try {
-        var nowDate = new Date(Date.now());
-
-        // TODO: Comprobar que existe un usuario autenticado y
-        // obtener su ID.
-        let fakeUserId = "1";
-
-        // Obtiene de la base de datos el meeting que se
-        // pretende abandonar.
-        let doc = await Meeting.findById(meetingId)
-            .select("_id name description address province postalCode startingDate endingDate capacity creatorId members")
-            .exec();
-        
-        // Comprobar que existe un meeting con la ID proporcionada.
-        if (!doc) {
-            throw "Error 404: We couldn't find any meeting with the given ID."
-
-        // Comprobar que la ID del usuario que está intentando
-        // abandonar el meeting no coincide con su creatorId.
-        } else if (doc.creatorId.toString().localeCompare(fakeUserId) == 0) {
-            throw "Error 400: You can't leave a meeting that you created.";
-
-        // Comprobar que la startingDate del meeting es futura
-        // y, por tanto, dicho meeting no ha comenzado.
-        } else if (doc.startingDate <= nowDate) {
-            throw "Error 400: You can't leave the meeting. It has already started.";
-
-        // Comprobar que la ID del usuario autenticado se
-        // encuentra en el listado de members del meeting.
-        } else if (!doc.members.includes(fakeUserId)) {
-            throw "Error 400: You can't leave a meeting that you are not a member of.";
-        
-        } else {
-            // Eliminar la ID del usuario autenticado del listado
-            // de members del meeting.
-            var newMembers = doc.members;
-            newMembers.splice(newMembers.indexOf(fakeUserId), 1);
-
-            // Actualizar el meeting con su nuevo listado de
-            // members en la base de datos.
-            let updatedMeeting = await Meeting.findByIdAndUpdate(meetingId, {members: newMembers});
-            console.log(updatedMeeting);
-
-            // Llamada al microservicio de Profile para actualizar
-            // las dependencias y reflejar el abandono del usuario
-            // del meeting.
-            await profileAxios.put('user/' + fakeUserId + '/leavesMeeting/' + meetingId);
-
-            return [false, updatedMeeting];
-        }
-    
-    } catch (error) {
-        return [true, error];
-    }
-}
-
 router.delete('/leave/:meetingId', (req, res, next) => {
 
     // Comprobar que se pasa una ID válida como parámetro.
@@ -362,7 +229,7 @@ router.delete('/leave/:meetingId', (req, res, next) => {
         // Llamada al método asíncrono principal que se encarga
         // de eliminar al usuario del meeting y actualizar los
         // datos en el microservicio Profile
-        leaveMeeting(meetingId).then(doc => {
+        meetingService.leaveMeeting(meetingId).then(doc => {
 
             // Comprobar si se ha producido un error (doc[0] = true) y
             // enviar el código y mensaje adecuados.
---
+++ src/services/meetingService.js
@@ -0,0 +1,144 @@
+const axios = require('axios');
+
+const profileAxios = axios.create({
+    baseURL: 'http://localhost:3001/api/'
+});
+const Meeting = require('../models/meeting');
+
+module.exports = {
+    getMeetingsFromUser: async function (userId) {
+
+        try {
+            // Llamada a la API del microservicio Profile para
+            // obtener el listado de IDs de los meetings del
+            // usuario.
+            console.log("HOLA SOY " + userId);
+            let doc = await profileAxios.get('user/' + userId + '/joinedMeetings');
+            console.log(doc);
+            let meetingsIds = doc.data;
+    
+            // Obtiene de la base de datos los meetings cuyo
+            // ID se encuentra en el listado obtenido anteriormente.
+            let meetingsCollection = await Meeting.find().where('_id').in(meetingsIds)
+                .select("_id name description address province postalCode startingDate endingDate capacity creatorId members")
+                .exec();
+            
+            return [false, meetingsCollection];
+        
+        } catch (error) {
+    
+            return [true, error];
+        }
+    },
+
+    deleteMeeting: async function (meetingId) {
+
+        try {
+            var nowDate = new Date(Date.now());
+    
+            // TODO: Comprobar que existe un usuario autenticado y
+            // obtener su ID.
+            let fakeUserId = "1";
+    
+            // Obtiene de la base de datos el meeting que se
+            // pretende eliminar.
+            let doc = await Meeting.findById(meetingId)
+                .select("_id name description address province postalCode startingDate endingDate capacity creatorId members")
+                .exec();
+            
+            // Comprobar que existe un meeting con la ID proporcionada.
+            if (!doc) {
+                throw "Error 404: We couldn't find any meeting with the given ID."
+    
+            // Comprobar que la ID del usuario que está intentando
+            // abandonar el meeting coincide con su creatorId.
+            } else if (doc.creatorId.toString().localeCompare(fakeUserId) != 0) {
+                throw "Error 400: You can't delete a meeting that you didn't create.";
+    
+            // Comprobar que la startingDate del meeting es futura
+            // y, por tanto, dicho meeting no ha comenzado.
+            } else if (doc.startingDate <= nowDate) {
+                throw "Error 400: You can't delete the meeting. It has already started.";
+            
+            } else {
+                // El listado de miembros del meeting recoge las dependencias
+                // con los usuarios que se deben actualizar en la base de datos
+                // del microservicio profile.
+                var members = doc.members;
+    
+                // Eliminamos el meeting de nuestra base de datos.
+                await Meeting.deleteOne({_id : meetingId}).exec();
+                
+                // Actualizamos las dependencias del microservicio profile,
+                // quitando el meeting eliminado para cada uno de los miembros
+                // en sus respectivos listados de meetings. 
+                for (var i = 0; i < members.length; i++) {
+                    await profileAxios.put('user/' + members[i] + '/leavesMeeting/' + meetingId);
+                }
+    
+                return [false, members];
+            }
+        
+        } catch (error) {
+            return [true, error];
+        }
+    },
+
+    leaveMeeting: async function (meetingId) {
+
+        try {
+            var nowDate = new Date(Date.now());
+    
+            // TODO: Comprobar que existe un usuario autenticado y
+            // obtener su ID.
+            let fakeUserId = "1";
+    
+            // Obtiene de la base de datos el meeting que se
+            // pretende abandonar.
+            let doc = await Meeting.findById(meetingId)
+                .select("_id name description address province postalCode startingDate endingDate capacity creatorId members")
+                .exec();
+            
+            // Comprobar que existe un meeting con la ID proporcionada.
+            if (!doc) {
+                throw "Error 404: We couldn't find any meeting with the given ID."
+    
+            // Comprobar que la ID del usuario que está intentando
+            // abandonar el meeting no coincide con su creatorId.
+            } else if (doc.creatorId.toString().localeCompare(fakeUserId) == 0) {
+                throw "Error 400: You can't leave a meeting that you created.";
+    
+            // Comprobar que la startingDate del meeting es futura
+            // y, por tanto, dicho meeting no ha comenzado.
+            } else if (doc.startingDate <= nowDate) {
+                throw "Error 400: You can't leave the meeting. It has already started.";
+    
+            // Comprobar que la ID del usuario autenticado se
+            // encuentra en el listado de members del meeting.
+            } else if (!doc.members.includes(fakeUserId)) {
+                throw "Error 400: You can't leave a meeting that you are not a member of.";
+            
+            } else {
+                // Eliminar la ID del usuario autenticado del listado
+                // de members del meeting.
+                var newMembers = doc.members;
+                newMembers.splice(newMembers.indexOf(fakeUserId), 1);
+    
+                // Actualizar el meeting con su nuevo listado de
+                // members en la base de datos.
+                let updatedMeeting = await Meeting.findByIdAndUpdate(meetingId, {members: newMembers});
+                console.log(updatedMeeting);
+    
+                // Llamada al microservicio de Profile para actualizar
+                // las dependencias y reflejar el abandono del usuario
+                // del meeting.
+                await profileAxios.put('user/' + fakeUserId + '/leavesMeeting/' + meetingId);
+    
+                return [false, updatedMeeting];
+            }
+        
+        } catch (error) {
+            return [true, error];
+        }
+    }
+}
