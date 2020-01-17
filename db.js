490dd6f5e2c9db5218ae0b80dd83093c2f89c3f3
Alicia <aliviaord@alum.us.es>
2020-01-17 13:29:11 +0100

    Integration tests updated

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const DB_URL = (process.env.MONGO_URL || 'mongodb+srv://admin:admin@animea-meeti
 const dbConnect = function() {
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
-    return mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+    return mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 }
 
 module.exports = dbConnect;
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,24 +1,27 @@
 const request = require('supertest');
-const Meeting = require('../src/models/meeting');
-const app = require('../server');
-const nock = require('nock');
-const authResponses = require('./authResponses');
-const BASE_API_PATH = '/api/v1';
 
-beforeEach(() => {
-  nock(`https://animea-gateway.herokuapp.com`)
-      .get('/auth/api/v1/auth/me')
-      .reply(200, JSON.stringify(authResponses.verifyToken));
+const app = require('../server.js');
+
+describe("Hello world tests", () => {
+
+    it("Should do an stupid test", () => {
+        const a = 5;
+        const b = 3;
+        const sum = a + b;
+
+        expect(sum).toBe(8);
+    });
+
 });
 
-describe('Get all meetings', () => {
-  it('Should return a list of 15 meetings', (done) => {
-    request(app)
-        .get(BASE_API_PATH + '/meetings')
-        .expect((response) => {
-          expect(response.statusCode).toBe(200);
-          expect(response.body.length).toEqual(15);
-        })
-        .expect(200, done);
-  });
+describe("Meetings API", () => {
+    describe("GET /", () => {
+        it("Should return an HTML document", () => {
+            return request(app).get("/").then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.type).toEqual(expect.stringContaining("html"));
+                expect(response.text).toEqual(expect.stringContaining("Welcome to express!"));
+            });
+        });
+    });
 });
